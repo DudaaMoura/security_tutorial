c42e1839fc6ac9a4c6db3466e88b576ca36f27bd
DudaaMoura <mariamourapalmital@gmail.com>
2023-11-08 17:15:15 +0000

    xsb

--- app/src/main/java/application/config/SecurityConfig.java
+++ app/src/main/java/application/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,7 +17,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf().disable()
-            .authorizeHTTpRequest()
+            .authorizeHttpRequest()
             .anyRequest().authenticated();
         http    .formLogin();
 
